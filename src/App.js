31766ba848207c65abdcdfaabe02b16d8bc3735f
Mayank Sharma <55140492+MayankGeek@users.noreply.github.com>
2024-05-26 00:33:30 +0530

    added code to render gltf model in browser

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function Model() {
+  const { scene } = useGLTF('/models/1505973.gltf');
+  return <primitive object={scene} />;
+}
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <Model />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
